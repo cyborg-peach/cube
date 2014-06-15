df6e1ac9c546aec026f1378d084ae5e664fbb08c
Squirrel <example@example.com>
2014-06-15 14:01:36 -0700

    using makefile to build and deploy example sensor app

---
+++ build-uno/m.d
@@ -0,0 +1,13 @@
+build-uno/m.o: m.ino \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Arduino.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/binary.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WCharacter.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/WString.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Print.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/Printable.h \
+ /usr/share/arduino/hardware/arduino/cores/arduino/new.h \
+ /usr/share/arduino/hardware/arduino/variants/standard/pins_arduino.h \
+ Wire.h /usr/share/arduino/hardware/arduino/cores/arduino/Stream.h \
+ MPL3115A2.h HTU21D.h
